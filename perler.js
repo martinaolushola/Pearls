d284fd205d2f9beb1287ea828847e0648dda296c
martinaolushola <martinaolushola@gmail.com>
2024-10-11 00:00:36 +0200

    Add files via upload
    plsss må jeg være færdig

--- perler.js
+++ perler.js
@@ -1,17 +1,15 @@
 // Section 1, billede 2
 function uni2normal(pic) {
-    pic.setAttribute("src", "Images/the-pearls/liked/galaxy-1.jpeg");
+    pic.setAttribute("src", "liked/galaxy-1.jpeg");
 }
 function uni2other(pic) {
-    pic.setAttribute("src", "Images/the-pearls/liked/galaxy-2.jpeg");
+    pic.setAttribute("src", "liked/galaxy-2.jpeg");
 }
 
 // Section 1, billede 4
 function uni4normal(pic) {
-    pic.setAttribute("src", "Images/the-pearls/liked/planets-2.jpeg");
+    pic.setAttribute("src", "liked/planets-2.jpeg");
 }
 function uni4other(pic) {
-    pic.setAttribute("src", "Images/the-pearls/liked/planets-3.jpeg");
+    pic.setAttribute("src", "liked/planets-3.jpeg");
 }
-
-
